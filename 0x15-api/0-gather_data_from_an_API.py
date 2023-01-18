203cc1e67dcdada07c45264a5c228268863e753a
george <georgev421@gmail.com>
2023-01-19 00:50:28 +0100

    task 1 api

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,35 +1,38 @@
 #!/usr/bin/python3
-'''Gather data from an API'''
-import json
-import sys
-import urllib.request
-
-def get_todo_list(employee_id):
-    url = 'https://jsonplaceholder.typicode.com/todos?userId={}'.format(employee_id)
-    response = urllib.request.urlopen(url)
-    todo_list = json.loads(response.read())
-    return todo_list
-
-def get_employee_name(employee_id):
-    url = 'https://jsonplaceholder.typicode.com/users/{}'.format(employee_id)
-    response = urllib.request.urlopen(url)
-    employee = json.loads(response.read())
-    return employee['name']
-
-def print_todo_list(employee_id):
-
-    todo_list = get_todo_list(employee_id)
-    employee_name = get_employee_name(employee_id)
-    done_tasks = [task for task in todo_list if task['completed']]
-    total_tasks = len(todo_list)
-    done_tasks_count = len(done_tasks)
-
-    print(f'Employee {employee_name} is done with tasks({done_tasks_count}/{total_tasks}):')
-    for task in done_tasks:
-        print(f'\t{task["title"]}')
+"""returns information for a given employee ID"""
+import requests
+from sys import argv
 
 
-if __name__ == '__main__':
-    employee_id = int(sys.argv[1])
-    print_todo_list(employee_id)
+def show_id():
+    """method returns information on employee TODO list"""
+    users = requests.get("https://jsonplaceholder.typicode.com/users")
+    for user in users.json():
+        """returns users as a json file"""
+        if user.get('id') == int(argv[1]):
+            EMPLOYEE_NAME = (user.get('name'))
+            break
+    TOTAL_NUM_OF_TASKS = 0
+    NUMBER_OF_DONE_TASKS = 0
+    TASK_TITLE = []
 
+    todos = requests.get("https://jsonplaceholder.typicode.com/todos")
+    for todo in todos.json():
+        """get a dictionary of todos"""
+        if todo.get('userId') == int(argv[1]):
+            TOTAL_NUM_OF_TASKS += 1
+            if todo.get('completed') is True:
+                NUMBER_OF_DONE_TASKS += 1
+                TASK_TITLE.append(todo.get('title'))
+    print("Employee {} is done with tasks({}/{}):".format(EMPLOYEE_NAME,
+                                                          NUMBER_OF_DONE_TASKS,
+                                                          TOTAL_NUM_OF_TASKS))
+    for task in TASK_TITLE:
+        print("\t {}".format(task))
+
+
+if __name__ == "__main__":
+    if len(argv) > 1:
+        show_id()
+    else:
+        print("You must add a UserId!")
